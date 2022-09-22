0427a4a1725adea1670b59ebbf39fe83faabbc64
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:45:53 +0100

    Create ejercicio9.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio9.js
@@ -0,0 +1,15 @@
+/* 
+
+9. Diseñar el código para que con un valor recibido 'x'  
+muestre los siguientes mensajes:
+
+a) ‘Este es muy fácil... ¡prueba otro!’, si x es 0.
+
+b) ‘El número es par’, si x es 2, 4 ó 6.
+
+c) ‘El número es impar’, si x es 1, 3 ó 5.
+
+d) ‘¡¡Sólo sé contar de 0 a 6!!’, para los demás casos.
+
+*/
+
