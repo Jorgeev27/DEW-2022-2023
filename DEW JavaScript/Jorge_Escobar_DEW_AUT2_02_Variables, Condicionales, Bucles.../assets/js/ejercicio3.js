e4c95df398a5aed5d682b197cf809941c23a17ac
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:42:33 +0100

    Create ejercicio3.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio3.js
@@ -0,0 +1,13 @@
+/* 
+
+3. Anida dos bucles for y muestra por la consola el resultado de los 
+dos índices en cada iteración. Realiza 10 iteraciones en cada bucle.
+
+*/
+
+for(i = 1; i <= 11; i++){
+    console.log(i);
+    for(i = 1; i <=11; j++){
+        console.log(j);
+    }
+}
