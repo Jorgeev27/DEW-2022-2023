6cb9d9af34895fb6bd37f4da1c55c76a80cd075c
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:47:14 +0100

    Create ejercicio13.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio13.js
@@ -0,0 +1,12 @@
+/* 
+
+13. Escribir un programa que proporcione el desglose en el número 
+mínimo de billetes y monedas de una cantidad entera cualquiera 
+de euros dada. Recuerda que los billetes y monedas de uso legal 
+disponibles hasta 1 euro son de: 500, 200, 100, 50, 20, 10, 5, 2 y 1 euros. 
+Para ello deben solicitar al usuario un número entero 
+(comprobar que lo es) y desglosar la cantidad en el número 
+mínimo de billetes y monedas.
+
+*/
+
