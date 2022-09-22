5b1d2d2b2afbc35810a5277dba1c92acc969a3be
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:46:41 +0100

    Create ejercicio11.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio11.js
@@ -0,0 +1,9 @@
+/* 
+
+11. Para convertir grados Farenheit (`F`) a Celsius (`C`) se usa la 
+siguiente expresión: `C = (F − 32) * 5/9`. Implementar una función 
+que realice esta conversión (comprueba que 32ºF son 0ºC 
+y que 75.2ºF son 24ºC).
+
+*/
+
