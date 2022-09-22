919767a610fcab023bc27be82087cda679a9eb94
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:45:01 +0100

    Create ejercicio7.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio7.js
@@ -0,0 +1,29 @@
+/* 
+
+7. Hacer un juego para que el usuario averigüe la capital de un país 
+previamente definida. Tiene tres intentos y debe mostrase en 
+pantalla si ha acertado o no, incluyendo en este caso el 
+número de intentos que le quedan.
+
+*/
+
+var contadorFallo = 2;
+var capitalPais = "Bruselas";
+var acierto = false;
+var intro = alert("Te voy a decir un pais y tu me tienes que adivinar la capital");
+while(contadorFallo >=0 && !acierto){
+    var respuestaCapital = prompt("Capital de... Belgica");
+    if(respuestaCapital == capitalPais){
+        acierto = true;
+        alert("Felicidades!!!! Has acertado");
+    }else{
+        alert("OOH, has fallado pero tienes " + contadorFallo + " intentos mas");
+        contadorFallo --;
+    }
+}
+
+if(acierto == true){
+    alert("Aqui acaba al juego con la respuesta correcta!!!!");
+}else{
+    alert("Has terminado con los intentos NOOOO. La respuesta es: " + capitalPais);
+}
