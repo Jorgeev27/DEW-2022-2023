2bd47fdb442409fce23763e4368f62fb33ed0d35
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:46:58 +0100

    Create ejercicio12.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio12.js
@@ -0,0 +1,9 @@
+/* 
+
+12. En el ejercicio anterior, añadir un argumento opcional booleano 
+de forma que si está a `False` (valor por defecto) devuelve sólo 
+el resultado numérico (p.ej: `24`), mientras que si se indica `True` 
+le añade la unidad `ºC` (p.ej `24ºC`).
+
+*/
+
