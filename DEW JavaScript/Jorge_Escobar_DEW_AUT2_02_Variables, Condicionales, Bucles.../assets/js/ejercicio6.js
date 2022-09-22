e82b3f82b91e646e3858f12d8079432fb2e95150
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:44:45 +0100

    Create ejercicio6.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio6.js
@@ -0,0 +1,15 @@
+/* 
+
+6. En este reto deberás sumar el valor de 5 números naturales. 
+Para ello, deberás solicitar al usuario que introduzca un 
+número entero, calcular y sumar los 5 números siguientes e 
+imprimir el resultado por la consola, y volver a repetir 
+este proceso 4 veces más.
+*/
+
+for(i = 0; i < 5; i++){
+    var num = parseInt(prompt("Introduce un numero"));
+    var numMas5 = alert("Estos son los 5 numeros siguientes al numero introducido: " + "("+ num + ") " + 
+    parseInt(num +1) + "-" + parseInt(num +2) + "-" + parseInt(num +3) + "-" + parseInt(num +4) + "-" + parseInt(num +5));
+    console.log(numMas5);
+}
