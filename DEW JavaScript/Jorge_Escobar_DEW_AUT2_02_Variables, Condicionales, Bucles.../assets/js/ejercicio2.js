0e6dba5c58f3fa65c7da0c5fbc4be244c826aa66
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:42:13 +0100

    Create ejercicio2.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio2.js
@@ -0,0 +1,14 @@
+/* 
+
+2. Usando un bucle, calcula la suma de los 10 primeros números naturales, 
+es decir, la suma de 1 + 2 + 3 ... + 10, e imprimir el resultado por la 
+consola. Para ello deberás recorrer el bucle y almacenar el valor 
+acumulado en cada iteración en una variable.
+
+*/
+
+var num = 0;
+for(i = 1; i<=10; i++){
+    num += i;
+}
+console.log(num);
