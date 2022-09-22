0b99b92bdc6b00aa5e6148b0bc03b963081fe570
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:41:38 +0100

    Create ejercicio1.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio1.js
@@ -0,0 +1,12 @@
+/* 
+
+1. Usando un bucle, calcula el resultado de 15 elevado a la 6. 
+No podrás utilizar el operador aritmético de elevar a una potencia.
+
+*/
+
+var num = 15;
+for (a = 1; a <= 6; a++){
+   var resultado = num**a;
+}
+console.log(resultado);
