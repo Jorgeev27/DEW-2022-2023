bb704e5d600c05613dd2324f5943faf5b9d77f44
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-22 12:42:56 +0100

    Create ejercicio4.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio4.js
@@ -0,0 +1,16 @@
+/* 
+
+4. En este reto deberás mostrar el número posterior a un número 
+introducido por el usuario. Deberás solicitar al usuario que 
+introduzca 3 números, y para cada uno de ellos imprimir 
+el número posterior.
+
+*/
+
+var numero1 = prompt("Introduzca el primer número");
+var numero2 = prompt("Introduzca el segundo número");
+var numero3 = prompt("Introduzca el tercer número");
+
+console.log(parseInt(numero1) +1);
+console.log(parseInt(numero2) +1);
+console.log(parseInt(numero3) +1);
