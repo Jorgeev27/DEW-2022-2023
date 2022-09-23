fe50085b512173374d53f044062001e3b27ab81b
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-23 12:26:18 +0100

    Update ejercicio1.js

--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio1.js
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio1.js
@@ -7,6 +7,6 @@ No podrás utilizar el operador aritmético de elevar a una potencia.
 
 let num = 15;
 for (a = 1; a <= 6; a++){
-   let resultado = num**a;
+   let resultado = num ** a;
+   console.log(resultado);
 }
-console.log(resultado);
