be50a6db43bb051475ef97367264d4a9a4ab620d
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-23 12:31:59 +0100

    Update ejercicio14.js

--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio14.js
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_02_Variables, Condicionales, Bucles.../assets/js/ejercicio14.js
@@ -1,7 +1,7 @@
 /* 
 
 14. ¿Cuántos segundos han pasado desde que naciste? ¿y minutos? 
-¿y horas? ¿y días? Implementaruna función que dada una fecha cualquiera 
+¿y horas? ¿y días? Implementar una función que dada una fecha cualquiera 
 en formato “yyyy-mm-dd”, devuelva cuánto tiempo ha pasado desde esa 
 fecha. La función aceptará un segundo parámetro para indicar en qué 
 unidad se quiere obtener el 
@@ -10,3 +10,20 @@ Si no es ninguna de estas unidades, se mostrará un error.
 
 */
 
+let fechaNacimiento = prompt("Introduce tu fecha de nacimiento: formato yyyy-mm-dd");
+fechaEnSegMinHD(fechaNacimiento);
+
+function fechaEnSegMinHD(fechaSegMinHD){
+    let fechaActual = new Date();
+    fecha =  fechaActual.getFullYear() + "/" + (fechaActual.getMonth() + 1) + fechaActual.getDate();
+    let fechaSegundos = parseInt(fechaActual.getTime());
+    let fechaMinutos = parseInt(fechaSegundos / 60);
+    let fechaHoras = parseInt(fechaMinutos / 60);
+    let fechaDias = parseInt(fechaHoras / 24);
+    fechaSegMinHD = fechaSegundos, fechaMinutos, fechaHoras, fechaDias;
+
+    document.write("Pasaron " + fechaSegundos + " segundos desde tu fecha de nacimiento <br/>");
+    document.write("Pasaron " + fechaMinutos + " minutos desde tu fecha de nacimiento <br/>");
+    document.write("Pasaron " + fechaHoras + " horas desde tu fecha de nacimiento <br/>");
+    document.write("Pasaron " + fechaDias + " dias desde tu fecha de nacimiento <br/>");
+}
