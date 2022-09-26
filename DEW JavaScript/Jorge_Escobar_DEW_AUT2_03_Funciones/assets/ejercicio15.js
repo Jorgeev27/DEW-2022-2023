3141bb71fa903e252dd1e5358bb7311818ba2ac3
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-26 15:41:15 +0100

    Delete ejercicio15.js

--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_03_Funciones/assets/ejercicio15.js
+++
@@ -1,8 +0,0 @@
-/* 
-
-15. Programa que tenga una función que le pasemos un vector de N elementos 
-y rota sus componentes un lugar hacia su derecha. Teniendo en 
-cuenta que el último componente debe desplazarse al primer lugar.
-
-*/
-
