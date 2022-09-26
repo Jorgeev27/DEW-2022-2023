6fd46b6c9d078d41b460d9a3102356e6a27cd35a
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-26 15:38:52 +0100

    Create ejercicio13.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_03_Funciones/assets/js/ejercicio13.js
@@ -0,0 +1,10 @@
+/* 
+
+13. Haz un programa que dado un número del 1 al 99, 
+lo muestre escrito en letras. Por ejemplo, 
+dado el 35 debería mostrarse treinta y cinco.
+
+*/
+
+let numero = prompt("Dime un numero del 1 al 99");
+
