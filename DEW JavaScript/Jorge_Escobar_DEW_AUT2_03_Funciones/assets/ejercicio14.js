a2c4b69e26be22646ad9b64f8e5cbabf3042e66c
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-26 15:31:51 +0100

    Create ejercicio14.js

---
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_03_Funciones/assets/ejercicio14.js
@@ -0,0 +1,9 @@
+/* 
+
+14. Programa que lee una frase y averigua si es “palíndroma”. 
+Una frase es palíndroma si se lee igual de izquierda a derecha 
+que de derecha a izquierda (sin considerar los espacios en blanco). 
+Ejemplo: “le saco sus ocas el”, “dabale arroz a la zorra el abad”.
+
+*/
+
