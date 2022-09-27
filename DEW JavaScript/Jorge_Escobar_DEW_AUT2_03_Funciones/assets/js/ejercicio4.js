37d11bb1ea1a37d3ac6c5c90d35a5a638f2a768e
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-09-27 22:38:47 +0100

    Update ejercicio4.js

--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_03_Funciones/assets/js/ejercicio4.js
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_03_Funciones/assets/js/ejercicio4.js
@@ -1,18 +1,27 @@
-/*
+/* 
 
 4. En el siguiente bucle while tenemos que tener cuidado 
 porque hay operaciones que pueden dar error 
 (no puede haber raíces cuadradas de números 
     negativos, ni divisiones por cero).
+
     let result = 1, arg1, arg2, res1, res2;
     while (result > 0) {
+
         arg1 = Math.random() - 0.5;
+
         res1 = Math.sqrt(arg1);
+
         arg2 = Math.random();
+
         res2 = res1/(arg1 + arg2);
+
         result = res1 + res2;
+
         console.log(result);
+
     }
+
 a) Cambia el bucle while usando continue y break para que si 
 hay una raíz cuadrada de un número negativo directamente 
 pase a la siguiente iteración. En el caso de haber una 
@@ -25,3 +34,32 @@ c) Cambia el bucle while original para que se ejecute sin
 tener que asignar un valor inicial a la variable result.
 
 */
+
+let result = 1, arg1, arg2, res1, res2;
+
+//C:
+let valor = false;
+
+while(valor == true){
+    arg1 = Math.random() - 0.5;
+
+    if(arg1 > 0){
+        console.log("ERROR!!");
+        //B: 
+        break;
+    }else{
+        res1 = Math.sqrt(arg1);
+    }
+
+    arg2 = Math.random();
+    if(arg1 + arg2 == 0){
+        //A:
+        console.log("ERROR!!");
+        continue;
+    }else{
+        res2 = res1 / (arg1 + arg2);
+    }
+    result = res1 + res2;
+    valor = false;
+    console.log(result);
+}
