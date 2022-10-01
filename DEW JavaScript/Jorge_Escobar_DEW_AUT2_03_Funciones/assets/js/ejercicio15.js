2b2a0a80fefcff86200a56859c6519abc5ef0022
Jorge <jorgeescobarv27@gmail.com>
2022-10-01 11:39:15 +0100

    Main

--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_03_Funciones/assets/js/ejercicio15.js
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_03_Funciones/assets/js/ejercicio15.js
@@ -6,4 +6,19 @@ cuenta que el último componente debe desplazarse al primer lugar.
 
 */
 
+let elementos = parseInt(prompt("¿Cuantos elementos quieres en el vector?"));
+let listaElementos = Array(elementos);
+vector(listaElementos);
+document.write(listaElementos);
 
+function vector(matrizElementos){
+    let largo, valor;
+    largo = matrizElementos.length;
+    for (let i = 0; i < largo; i++){
+        valor = Math.random()*10;
+        matrizElementos[i] = Math.round(valor);
+    }
+    listaElementos.pop();
+    listaElementos.unshift();
+    console.log(listaElementos);
+}
