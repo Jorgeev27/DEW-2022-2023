7e4f672b159ca0552f088b3437000f6f247f39d4
Jorge <jorgeescobarv27@gmail.com>
2022-10-10 22:43:34 +0100

    Main

--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_05_Primeros_DOM/assets/js/ejercicio4.js
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_05_Primeros_DOM/assets/js/ejercicio4.js
@@ -19,56 +19,59 @@ Realiza:
  */
 
  function elemento4(){
-  if(!document.getElementById("cuatro")){ // Si no está el id "cuatro"
-      let tr = document.createElement("tr"); // Crea un elemento "tr"
-      let td = document.createElement("td"); // Crea un elemento "td"
-      let tabla = document.getElementById("tabla"); // Seleccionamos el elemento id "tabla"
-      td.innerHTML = "4"; // Le ponemos texto y/o numero. En este caso, lo puse con numero
-      td.id = "3"; // Le pasamos el id "cuatro" que tiene que crear
-      td.onclick = function(){
-          descender(); // Y ponemos la función descender que hace que cuando pulsemos en la celda, dicho número descienda
-      }
-      tr.appendChild(td); // Añadimos el td
-      tabla.append(tr); // Añadimos el tr
-  }
+    if(!document.getElementById("cuatro")){ // Si no está el id "cuatro"
+        let tr = document.createElement("tr"); // Crea un elemento "tr"
+        let td = document.createElement("td"); // Crea un elemento "td"
+        let tabla = document.getElementById("tabla"); // Seleccionamos el elemento id "tabla"
+        td.innerHTML = "4"; // Le ponemos texto y/o numero. En este caso, lo puse con numero
+        td.id = "3"; // Le pasamos el id "cuatro" que tiene que crear
+        tr.appendChild(td); // Añadimos el td
+        tabla.append(tr); // Añadimos el tr
+    }
 }
 
 /**
-* Función que cuando se puse en la celda de algún número descienda.
-*/
+ * Función que cuando se puse en la celda de algún número descienda.
+ */
 
-function descender(){
-
+function descender(numero){
+    let celda;
+    let td = document.getElementsByTagName("td"); // Cogemos todos los elementos "td"
+    if(numero < td.length -1){
+        celda = td[numero + 1];
+        td[numero + 1] = td[numero];
+        td[numero] = celda;
+    }
 }
 
 /**
-* Función que contiene 2 soluciones:
-* Solución 1: Cada celda tiene un color distinto; dependiendo si la celda sea par o impar.
-*             Es decir, las celdas pares en este caso, tienen el color amarillo y las celdas impares de color naranja.
-* Solución 2: Cuando el ratón pasa por encima de las celdas pares e impares, tienen un color distinto al predeterminado.
-*             Es decir, las celdas pares si pasa el ratón, tendrán color rojo y las celdas impares de color azul.
-*             Una vez que el ratón haya pasado por las celdas, éstas vuelven a sus colores orginales (amarillo pares y naranja impares).
-*/
+ * Función que contiene 2 soluciones:
+ * Solución 1: Cada celda tiene un color distinto; dependiendo si la celda sea par o impar.
+ *             Es decir, las celdas pares en este caso, tienen el color amarillo y las celdas impares de color naranja.
+ * Solución 2: Cuando el ratón pasa por encima de las celdas pares e impares, tienen un color distinto al predeterminado.
+ *             Es decir, las celdas pares si pasa el ratón, tendrán color rojo y las celdas impares de color azul.
+ *             Una vez que el ratón haya pasado por las celdas, éstas vuelven a sus colores orginales (amarillo pares y naranja impares).
+ */
 
 function coloresParImparYRaton(){
-  let td = document.getElementsByTagName("td"); // Cogemos todos los elementos "td"
-  for (let i = 0; td.length; i++){ //Recorrer con for
-      if(i % 2 == 0){ // Si el elemento td % 2 == 0
-          td[i].style.backgroundColor = "orange"; // Color naranja
-          td[i].addEventListener("mouseover", function(event){
-              event.target.style.backgroundColor = "blue"; // Evento para que cuando pase el ratón sea de color azul
-          });
-          td[i].addEventListener("mouseout", function(event){
-              event.target.style.backgroundColor = "orange"; // Evento para que cuando deje de pasar el ratón, vuelva a su color original
-          });
-      }else{
-          td[i].style.backgroundColor = "yellow"; // Color amarillo
-          td[i].addEventListener("mouseover", function(event){
-              event.target.style.backgroundColor = "red"; // Evento para que cuando pase el ratón sea de color rojo
-          });
-          td[i].addEventListener("mouseout", function(event){
-              event.target.style.backgroundColor = "yellow"; // Evento para que cuando deje de pasar el ratón, vuelva a su color original
-          });
-      }
-  }
+    let td = document.getElementsByTagName("td"); // Cogemos todos los elementos "td"
+    for (let i = 0; td.length; i++){ //Recorrer con for
+        if(i % 2 == 0){ // Si el elemento td % 2 == 0
+            td[i].style.backgroundColor = "orange"; // Color naranja
+            td[i].addEventListener("mouseover", function(event){
+                event.target.style.backgroundColor = "blue"; // Evento para que cuando pase el ratón sea de color azul
+            });
+            td[i].addEventListener("mouseout", function(event){
+                event.target.style.backgroundColor = "orange"; // Evento para que cuando deje de pasar el ratón, vuelva a su color original
+            });
+        }else{
+            td[i].style.backgroundColor = "yellow"; // Color amarillo
+            td[i].addEventListener("mouseover", function(event){
+                event.target.style.backgroundColor = "red"; // Evento para que cuando pase el ratón sea de color rojo
+            });
+            td[i].addEventListener("mouseout", function(event){
+                event.target.style.backgroundColor = "yellow"; // Evento para que cuando deje de pasar el ratón, vuelva a su color original
+            });
+        }
+    }
 }
