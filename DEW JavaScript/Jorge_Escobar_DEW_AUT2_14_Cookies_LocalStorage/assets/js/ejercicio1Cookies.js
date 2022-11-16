a8e1df524461057de97ed3d42636bd73009d1206
Jorge <jorgeescobarv27@gmail.com>
2022-11-16 11:01:33 +0000

    Main

--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_14_Cookies_LocalStorage/assets/js/ejercicio1Cookies.js
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_14_Cookies_LocalStorage/assets/js/ejercicio1Cookies.js
@@ -1,19 +1,85 @@
-function verCookie(){
+/* Agregar detectores de eventos a los botones. */
+document.getElementById("verCookies").addEventListener("click", verCookies);
+document.getElementById("crearCookies").addEventListener("click", crearModificarCookies);
+document.getElementById("modificarCookies").addEventListener("click", crearModificarCookies);
+document.getElementById("leerCookies").addEventListener("click", leerCookies);
+document.getElementById("borrarCookies").addEventListener("click", borrarCookies);
 
+
+/**
+ * Muestra un cuadro de mensaje con las cookies actuales.
+ */
+function verCookies(){
+    alert("Cookies actuales: \n" + document.cookie);
 }
 
-function crearCookie(){
-    
+/**
+ * Le pide al usuario un nombre, un valor y una fecha de caducidad, y luego crea una cookie con esa
+ * información.
+ */
+function crearModificarCookies(){
+    var nombre = prompt("Dime tu nombre");
+    var valor = prompt("Dime tu valor");
+    var expiracion = parseInt(prompt("Dime el número de días de expiración"));
+    setCookie(nombre, valor, expiracion);
+    verCookies();
 }
 
-function modificarCookie(){
-    
+/**
+ * Solicita al usuario un nombre de cookie, luego usa la función getCookie para recuperar el valor de
+ * la cookie y finalmente muestra el valor en un cuadro de alerta.
+ */
+function leerCookies(){
+    var nombre = prompt("Dime el nombre de la cookie a leer");
+    var resultado = getCookie(nombre);
+    alert(resultado);
 }
 
-function leerCookie(){
-    
+/**
+ * Le pide al usuario el nombre de una cookie y luego la elimina.
+ */
+function borrarCookies(){
+    var nombre = prompt("Dime el nombre de la cookie a borrar");
+    deleteCookie(nombre);
 }
 
-function borrarCookie(){
-    
+/**
+ * Si desea eliminar una cookie, simplemente configúrela para que caduque en el pasado.
+ * @param nombre - El nombre de la cookie.
+ */
+function deleteCookie(nombre){
+    setCookie(nombre,"", 0);
+}
+
+/**
+ * Crea una cookie con el nombre y el valor pasados como parámetros, y establece la fecha de caducidad
+ * en la cantidad de días pasados como parámetro.
+ * @param nombre - El nombre de la cookie.
+ * @param valor - El valor de la cookie.
+ * @param expiracion - El número de días hasta que caduque la cookie.
+ */
+function setCookie(nombre, valor, expiracion){
+    var date = new Date();
+    date.setTime(date.getTime() + expiracion * 24 * 3600 * 1000);
+    var expiracion = "expires=" + date.toUTCString();
+    document.cookie = nombre + "=" + valor + ";" + expiracion + ";path=/";
+}
+
+/**
+ * Toma una cadena como argumento y devuelve el valor de la cookie con ese nombre.
+ * @param nombre - El nombre de la cookie que desea recuperar.
+ * @returns El valor de la cookie.
+ */
+function getCookie(nombre){
+    var nom = nombre + "=";
+    var array = document.cookie.split(";");
+    for(var i = 0; i < array.length; i++){
+        var c = array[i];
+        while(c.charAt(0) == " "){
+            c = c.substring(1);
+        }
+        if(c.indexOf(nombre) == 0){
+            return c.substring(nombre.length, c.length);
+        }
+    }
 }
--- DEW JavaScript/Jorge_Escobar_DEW_AUT2_14_Cookies_LocalStorage/assets/js/ejercicio1LocalStorage.js
+++ DEW JavaScript/Jorge_Escobar_DEW_AUT2_14_Cookies_LocalStorage/assets/js/ejercicio1LocalStorage.js
@@ -1,11 +1,60 @@
+/* Obtener los elementos del archivo HTML. */
+let saludar = document.getElementById("saludar");
+let contadores  = document.getElementById("contadores");
+
+/* Declarando dos variables, una para el contador y otra para el localStorage. */
+let contador = 0;
+let locaLStorage = window.localStorage;
+
+
+/* Está llamando a la función `iniciarSesion()` cuando se carga la página. */
+onload = () => {
+    iniciarSesion();
+}
+
+
+/**
+ * Si no hay nada en localStorage, pídale al usuario su nombre y guárdelo en localStorage. Si hay algo
+ * en localStorage, saluda al usuario por su nombre
+ */
+function iniciarSesion(){
+    if(locaLStorage.length === 0){
+        let nombre = prompt("Dime tu nombre");
+        if(nombre !== null){
+            locaLStorage.setItem('nombre', nombre);
+            saludar.innerHTML += "Bienvenido " + locaLStorage.getItem("nombre") + "!";
+        }
+    }else{
+        if(locaLStorage.getItem('nombre') !== null && locaLStorage.getItem('nombre') !== "" && saludar.value === undefined){
+            saludar.innerHTML += "Otra vez por aquí " + locaLStorage.getItem('nombre') + ", ¿Cómo le va?";
+        }
+    }
+}
+
+/**
+ * Cuando se hace clic en el botón, el contador aumenta en uno y el nuevo valor se agrega a la lista de
+ * contadores.
+ */
 function aumentarContador(){
-    
+    contador++;
+    contadores.innerHTML += contador + "\n";
 }
 
+/**
+ * Disminuye el valor de la variable contador en uno, y luego agrega el nuevo valor de contador al
+ * final del texto en el elemento con el id contadores.
+ */
 function disminuirContador(){
-    
+    if(contador > 0){
+        contador--;
+        contadores.innerHTML += contador + "\n";
+    }
 }
 
+/**
+ * Borra el almacenamiento local.
+ */
 function cerrarSesion(){
-    
+    alert("Adios " + locaLStorage.getItem('nombre'));
+    locaLStorage.clear();
 }
