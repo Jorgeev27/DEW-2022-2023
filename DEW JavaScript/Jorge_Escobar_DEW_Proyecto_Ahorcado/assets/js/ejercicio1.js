3368cb2d9f4b242f148c439368dfa737d5fe61b8
Jorge <jorgeescobarv27@gmail.com>
2022-11-18 12:38:07 +0000

    Main

--- DEW JavaScript/Jorge_Escobar_DEW_Proyecto_Ahorcado/assets/js/ejercicio1.js
+++ DEW JavaScript/Jorge_Escobar_DEW_Proyecto_Ahorcado/assets/js/ejercicio1.js
@@ -1,3 +1,55 @@
+/* Obtiene los elementos del archivo HTML. */
+let intentosJugadores = document.getElementById("intentosJugadores");
+let ahorcado = document.getElementById("ahorcado");
+let formLetraUsuario = document.getElementById("formLetraUsuario");
+let letraUsuario = document.getElementById("letraUsuario");
+let enviarLetra = document.getElementById("enviarLetra");
+
+
 class Juego{
-    
+    /* Declarando las variables. */
+    peliculasSeries;
+    jugadores;
+    vidas;
+
+    /**
+     * Una función constructora de la clase Juego.
+     * @param peliculasSeries - Se trata de una matriz de objetos que contienen la información de las
+     * películas y series que se utilizarán en el juego.
+     * @param jugadores - número de jugadores en la partida
+     * @param vidas - número de vidas
+     */
+    constructor(peliculasSeries, jugadores, vidas){
+        this.peliculasSeries = peliculasSeries;
+        this.jugadores = jugadores;
+        this.vidas = vidas;
+    }
+
+
+    //GETTER Y SETTER DE PELICULASSERIES
+    get getPeliculasSeries(){
+        return this.peliculasSeries;
+    }
+
+    set setPeliculasSeries(peliculasSeries){
+        this.peliculasSeries = peliculasSeries;
+    }
+
+    //GETTER Y SETTER DE JUGADORES
+    get getJugadores(){
+        return this.jugadores;
+    }
+
+    set setJugadores(jugadores){
+        this.jugadores = jugadores;
+    }
+
+    //GETTER Y SETTER DE VIDAS
+    get getVidas(){
+        return this.vidas;
+    }
+
+    set setVidas(vidas){
+        this.vidas = vidas;
+    }
 }
