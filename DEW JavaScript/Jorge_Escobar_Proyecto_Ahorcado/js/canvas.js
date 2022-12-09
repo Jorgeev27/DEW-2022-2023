eb7ca7dacf3f61828a9eb0a0af153c792bbcd37b
Jorge Escobar <59926762+Jorgeev27@users.noreply.github.com>
2022-12-09 11:16:47 +0000

    Add files via upload

---
+++ DEW JavaScript/Jorge_Escobar_Proyecto_Ahorcado/js/canvas.js
@@ -0,0 +1,47 @@
+let canvas, context, imagen;
+imagen = document.getElementById('imageView');
+context = imagen.getContext('2d');
+context.lineWidth = 5;
+
+context.strokeStyle = '#000000';
+context.beginPath();
+context.moveTo(256, 286);
+context.lineTo(253, 74);
+context.stroke();
+context.closePath();
+
+context.strokeStyle = '#000000';
+context.beginPath();
+context.moveTo(252, 72);
+context.lineTo(352, 72);
+context.stroke();
+context.closePath();
+
+context.strokeStyle = '#000000';
+context.beginPath();
+context.moveTo(349, 69);
+context.lineTo(349, 101);
+context.stroke();
+context.closePath();
+
+context.strokeStyle = '#000000';
+context.beginPath();
+context.moveTo(214, 283);
+context.lineTo(309, 285);
+context.stroke();
+context.closePath();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
