afe9a042647c85f1516df5af6ba82281816c5059
Jorge <jorgeescobarv27@gmail.com>
2023-02-07 19:16:19 +0000

    Main

--- DEW JavaScript/2º Trimestre/Angular/Jorge_Escobar_AUT4_04_Proyecto_Marvel/marvelFilms_proyecto/src/app/components/crear-pelicula-marvel/crear-pelicula-marvel.component.ts
+++ DEW JavaScript/2º Trimestre/Angular/Jorge_Escobar_AUT4_04_Proyecto_Marvel/marvelFilms_proyecto/src/app/components/crear-pelicula-marvel/crear-pelicula-marvel.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-crear-pelicula-marvel',
   templateUrl: './crear-pelicula-marvel.component.html',
-  styleUrls: ['./crear-pelicula-marvel.component.css']
 })
 export class CrearPeliculaMarvelComponent {
 
