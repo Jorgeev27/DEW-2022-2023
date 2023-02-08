2c18e1d6b41ab6f8adf71a0edd15a8398323609e
Jorge <jorgeescobarv27@gmail.com>
2023-02-08 16:53:21 +0000

    Main

--- DEW JavaScript/2º Trimestre/Angular/Jorge_Escobar_AUT4_04_Proyecto_Marvel/marvelFilms_proyecto/src/app/components/detalle-pelicula-marvel/detalle-pelicula-marvel.component.ts
+++ DEW JavaScript/2º Trimestre/Angular/Jorge_Escobar_AUT4_04_Proyecto_Marvel/marvelFilms_proyecto/src/app/components/detalle-pelicula-marvel/detalle-pelicula-marvel.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Pelicula } from 'src/app/model/pelicula';
 
 @Component({
   selector: 'app-detalle-pelicula-marvel',
   templateUrl: './detalle-pelicula-marvel.component.html',
 })
-export class DetallePeliculaMarvelComponent {
+export class DetallePeliculaMarvelComponent implements OnInit {
 
+  ngOnInit(): void {
+    
+  }
 }
