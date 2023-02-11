aa90e857ad1eae5fe38edfd85c5f059e51305e97
Jorge <jorgeescobarv27@gmail.com>
2023-02-11 19:23:07 +0000

    Main

--- DEW JavaScript/2º Trimestre/Angular/Jorge_Escobar_AUT4_04_Proyecto_Marvel/marvelFilms_proyecto/src/app/pipes/caracterEspecialPelicula.pipe.ts
+++ DEW JavaScript/2º Trimestre/Angular/Jorge_Escobar_AUT4_04_Proyecto_Marvel/marvelFilms_proyecto/src/app/pipes/caracterEspecialPelicula.pipe.ts
@@ -1,13 +1,13 @@
 /* Importación de los módulos Pipe y PipeTransform de la biblioteca principal de Angular. */
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 /* Un decorador que nos permite definir el nombre de la pipe. */
 @Pipe({
-    name: 'caracterEspecialPeliculaMarvel'
+    name: 'caracterEspecialPelicula'
 })
 
 /* La clase CaracterEspecialPeliculaPipe implementa el método de transformación. */
-export class CaracterEspecialPeliculaPipe implements PipeTransform {
+export class CaracterEspecialPeliculaPipe implements PipeTransform{
     /**
      * Toma una cadena, verifica si contiene dos puntos y, si los contiene, reemplaza los dos puntos
      * con un espacio y un paréntesis izquierdo, y luego agrega un paréntesis derecho al final de la cadena.
@@ -15,10 +15,10 @@ export class CaracterEspecialPeliculaPipe implements PipeTransform {
      * @returns Valor de la variable cadena.
      */
     transform(value: string) {
-        let cadena: string ;
+        let cadena: string;
         if(value.includes(":")){
-            cadena=value.replace(":"," (");
-            cadena=cadena.concat(")");
+            cadena = value.replace(":"," (");
+            cadena = cadena.concat(")");
             return cadena;
         }
         return value;
